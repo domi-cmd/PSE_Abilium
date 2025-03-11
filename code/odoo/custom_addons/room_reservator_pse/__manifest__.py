5c3c58c4bb4b9c7ca4b16492c27441b190744dee
domi-cmd <dokro@gmx.ch>
2025-03-11 13:52:41 +0100

    Added necessary config and launch files for the odoo server, aswell as a template app

---
+++ code/odoo/custom_addons/room_reservator_pse/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom Room Reservator IoT',
+    'version': '1.0',
+    'summary': 'A custom Odoo module',
+    'description': 'This is a custom Odoo module which allows for calendar feature and IoT connection.',
+    'category': 'Tools',
+    'author': 'PSE Abilium',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/custom_model_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
