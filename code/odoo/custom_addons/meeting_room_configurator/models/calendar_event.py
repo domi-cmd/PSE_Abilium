fd16104e7f5164227b5f922b5f9b294fd85c4187
domi-cmd <dokro@gmx.ch>
2025-04-07 16:39:21 +0200

    Fixed bug which allowed for room creation from outside room configuration (in room-rasp and calendar), added feature so that only valid (active rooms) are display as options

--- code/odoo/custom_addons/meeting_room_configurator/models/calendar_event.py
+++ code/odoo/custom_addons/meeting_room_configurator/models/calendar_event.py
@@ -4,4 +4,9 @@ class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
 
     # Add custom fields
-    meeting_room = fields.Many2one('meeting.room', string="Choose Meeting Room", required=True)
+    meeting_room = fields.Many2one(
+        'meeting.room', 
+        string="Choose Meeting Room", 
+        required=True,
+        domain=[('availability', '=', True)]  # Ensure only available rooms are shown
+    )
