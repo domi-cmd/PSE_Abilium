6c55d3b9f106995ba44a2683257721c385209000
domi-cmd <dokro@gmx.ch>
2025-04-07 17:45:18 +0200

    Removed leftover TODO's from code

--- code/odoo/custom_addons/meeting_room_configurator/models/room.py
+++ code/odoo/custom_addons/meeting_room_configurator/models/room.py
@@ -6,11 +6,9 @@ class MeetingRoom(models.Model):
 
     name = fields.Char(string='Room Name', required=True)
     capacity = fields.Integer(string='Capacity', required=True)
-    # TODO: Split "location" up into "Street", "City", and "Floor"
     street = fields.Char(string='Street')
     city = fields.Char(string='City')
     floor = fields.Char(string='Floor')
-     # TODO: Add a "Description" variable
     description = fields.Char(string='Description')
     availability = fields.Boolean(string='Available', default=True)
    
