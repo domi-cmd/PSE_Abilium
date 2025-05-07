0bffef1f86556340421c41d551cddad021131041
domi-cmd <dokro@gmx.ch>
2025-05-07 18:27:12 +0200

    Moved mqtt connector into a seperate file as to refactor rasproom.py

--- code/odoo/custom_addons/Abilium_Room_Booker/models/connection_configuration.py
+++ code/odoo/custom_addons/Abilium_Room_Booker/models/connection_configuration.py
@@ -7,6 +7,7 @@ import ssl
 from contextlib import contextmanager
 import json
 from odoo.exceptions import ValidationError #for constraints
+from . import mqtt_connector
 
 _logger = logging.getLogger(__name__)
 
@@ -17,60 +18,6 @@ except ImportError:
     HAS_MQTT = False
     _logger.warning("paho-mqtt library not installed. MQTT functionality disabled")
 
-
-class MqttConnectionManager:
-    """Singleton to manage MQTT connections across Odoo instances"""
-    _instance = None
-    _connections = {}
-    _lock = threading.RLock()
-    
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(MqttConnectionManager, cls).__new__(cls)
-            cls._instance._init()
-        return cls._instance
-    
-    def _init(self):
-        self._connections = {}
-        self._lock = threading.RLock()
-    
-    def register(self, connection_id, client, thread=None, publisher_thread=None):
-        with self._lock:
-            self._connections[connection_id] = {
-                'client': client,
-                'thread': thread,
-                'publisher_thread': publisher_thread,
-                'timestamp': time.time()
-            }
-    
-    def unregister(self, connection_id):
-        with self._lock:
-            if connection_id in self._connections:
-                conn = self._connections.pop(connection_id)
-                client = conn.get('client')
-                if client:
-                    try:
-                        if client.is_connected():
-                            client.disconnect()
-                        client.loop_stop()
-                    except Exception as e:
-                        _logger.error("Error disconnecting client: %s", e)
-                
-                pub_thread = conn.get('publisher_thread')
-                if pub_thread and pub_thread.is_alive():
-                    pub_thread.do_run = False  # Signal thread to stop
-
-                return True
-        return False
-    
-    def get_client(self, connection_id):
-        with self._lock:
-            conn = self._connections.get(connection_id)
-            return conn.get('client') if conn else None
-    
-    def is_connected(self, connection_id):
-        client = self.get_client(connection_id)
-        return client and client.is_connected()
 
 
 class RoomRaspConnection(models.Model):
@@ -168,7 +115,7 @@ class RoomRaspConnection(models.Model):
 
     @property
     def mqtt_manager(self):
-        return MqttConnectionManager()
+        return mqtt_connector.MqttConnectionManager()
 
     @contextmanager
     def _get_new_cursor(self):
@@ -581,7 +528,7 @@ class RoomRaspConnection(models.Model):
             ('active', '=', True)
         ])
         
-        manager = MqttConnectionManager()
+        manager = mqtt_connector.MqttConnectionManager()
         
         for connection in connections:
             try:
